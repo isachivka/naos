6808f5bd2c07a4eb47b4ad7bb3a5cbfa74aa4cea
Igor Sachivka <juicy.igor@gmail.com>
2021-04-27 20:42:00 +0300

    chore: Init repository

---
+++ commitizen.js
@@ -0,0 +1,13 @@
+const custom = require('@isachivka/cz-conventional-changelog-for-jira/configurable');
+const getPackages = require('get-monorepo-packages');
+
+const packages = getPackages('./');
+const names = packages.map(pkg => {
+  return pkg.location.split('/')[1];
+});
+
+module.exports = custom({
+  skipScope: false,
+  scopes: ['', ...names],
+  jiraOptional: true,
+});
---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-lerna-scopes'
+  ],
+  rules: {
+    "subject-case": [0]
+  }
+}
